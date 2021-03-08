d92da166c65b8171868d6de6e2e15f12215002ab
Yan Klyuzhin <klyuzhin@uchi.ru>
2021-03-08 17:44:04 +0300

    added main screen with hardcoded cards

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import Main from '../main/main';
+import PropTypes from 'prop-types';
 
-const App = () => {
-  return (
-    <div>
-
-    </div>
-  );
+const App = (props) => {
+  return <Main promoMovieInfo={props.promoMovieInfo} />;
 };
 
+App.propTypes = {
+  promoMovieInfo: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    meta: PropTypes.shape({
+      genre: PropTypes.string.isRequired,
+      year: PropTypes.number.isRequired
+    })
+  })
+};
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+const PROMO_MOVIE_INFO = {
+  title: `The Grand Budapest Hotel`,
+  meta: {
+    genre: `Drama`,
+    year: 2014
+  }
+};
+
 ReactDOM.render(
-    <App />,
+    <App promoMovieInfo={PROMO_MOVIE_INFO} />,
     document.querySelector(`#root`)
 );
